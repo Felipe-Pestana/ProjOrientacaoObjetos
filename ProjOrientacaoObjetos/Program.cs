f507396460e22a59dfd25130cecc9176bda122ae
Felipe Pestana <pestana@sitic.com.br>
2022-08-29 10:08:32 -0300

    Exemplo de uso de lista.

--- ProjOrientacaoObjetos/Program.cs
+++ ProjOrientacaoObjetos/Program.cs
@@ -1,82 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ProjOrientacaoObjetos
+namespace Ex_List
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Pessoas p1 = new Pessoas();
+            List<string> listaString = new List<string>();
+            string nome;
+            Console.WriteLine("Capacidade:" + listaString.Capacity);
+            do
+            {
+                Console.WriteLine("Digite um nome para inserir na lista:");
+                nome = Console.ReadLine();
+                listaString.Add(nome);
+            } while (nome != "");
 
-            p1.CadastrarPessoa();
+            Console.WriteLine("A lista tem " + listaString.Count + " elementos:");
+            //Imprime cada item da lista
+            listaString.ForEach(i => Console.WriteLine(i));
 
-            Console.WriteLine(p1.ImprimirCadastro());
+            //Ordenar
+            Console.WriteLine("A lista ORDENADA:");
+            listaString.Sort();
+            listaString.ForEach(i => Console.WriteLine(i));
+
+            //Acessando uma posição especifica
+            listaString[3] = "John Doe";
+
+            //Adicionar (Semelhante ao Add) em uma posição especifica, porém com posição definida
+            listaString.Insert(1, "Tiririca");
+
+            Console.WriteLine("A lista MODIFICADA:");
+            listaString.ForEach(i => Console.WriteLine(i));
+            Console.WriteLine("Capacidade:" + listaString.Capacity);
+
+            if (listaString.Remove("Vitor"))
+                Console.WriteLine("Elemento Removido com sucesso");
+            else
+                Console.WriteLine("Elemento não Localizado!");
+
+            Console.WriteLine("A lista MODIFICADA:");
+            listaString.ForEach(i => Console.WriteLine(i));
+
             Console.ReadKey();
 
-            Console.WriteLine("Nome: ");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Data de Nascimento: ");
-            DateTime data = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("CPF: ");
-            string cpf = Console.ReadLine();
-            Console.WriteLine("Telefone: ");
-            long telefone = long.Parse(Console.ReadLine());
-
-            Pessoas p2 = new Pessoas(nome, data, cpf, telefone);
-
-            Console.WriteLine(p2.ImprimirCadastro());
-            Console.ReadKey();
-
-            Console.WriteLine("Nome: ");
-            nome = Console.ReadLine();
-            Console.WriteLine("Data de Nascimento: ");
-            data = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("CPF: ");
-            cpf = Console.ReadLine();
-
-            Pessoas p3 = new Pessoas(nome, data, cpf);
-            
-            p3.ImprimirCadastro();
-            Console.ReadKey();
-
-            p3.setTelefone();
-
-            Console.WriteLine(p3.ImprimirCadastro());
-
-            /*Pessoas p2 = new Pessoas();
-            Pessoas p3 = new Pessoas();
-
-            p1.setNome();
-            p1.setDataNasc();
-            p1.setCpf();
-            p1.setTelefone();
-
-            p1.getNome();
-            p1.getDataNasc();
-            p1.getCpf();
-            p1.getTelefone();
-
-            p2.setNome();
-            p2.setDataNasc();
-            p2.setCpf();
-            p2.setTelefone();
-
-            p2.getNome();
-            p2.getDataNasc();
-            p2.getCpf();
-            p2.getTelefone();
-
-            p3.setNome();
-            p3.setDataNasc();
-            p3.setCpf();
-            p3.setTelefone();
-
-            p3.getNome();
-            p3.getDataNasc();
-            p3.getCpf();
-            p3.getTelefone();*/
-
-
+            //Referências
+            //remove()
+            //removeAt(posição)
+            //http://www.linhadecodigo.com.br/artigo/3676/listt-trabalhando-com-listas-genericas-em-csharp.aspx#ixzz6nJOXhelV
+            //https://www.alura.com.br/artigos/listas-em-csharp?gclid=Cj0KCQiA7NKBBhDBARIsAHbXCB46cB3ZHr1HydVeEe04lFZ3zj6u8ne_0tJnHJkanTjbXV1FHZ_nSZgaAqj4EALw_wcB
+            //https://www.caelum.com.br/apostila-csharp-orientacao-objetos/trabalhando-com-listas#exercicios
+            //https://docs.microsoft.com/pt-br/dotnet/api/system.collections.generic.list-1?view=net-5.0
+            //Definição de Coleção: https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/concepts/collections
         }
     }
 }
